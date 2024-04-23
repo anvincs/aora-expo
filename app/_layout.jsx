e303af3d6972d6301f935befac4ad5b9146d670f
Anvin.C.Shaju <anvincsg@gmail.com>
2024-04-23 12:33:07 +0530

    Global Context setup

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,7 +1,8 @@
-import { StyleSheet, Text, View } from "react-native";
 import { Slot, SplashScreen, Stack } from "expo-router";
 import { useFonts } from "expo-font";
 import { useEffect } from "react";
+
+import GlobalProvider from "../context/GlobalProvider";
 
 // const RootLayout = () => {
 //   return (
@@ -41,24 +42,26 @@ const RootLayout = () => {
   }
 
   return (
-    <Stack>
-      <Stack.Screen
-        name="index"
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen
-        name="(auth)"
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen
-        name="(tabs)"
-        options={{ headerShown: false }}
-      />
-      {/* <Stack.Screen
+    <GlobalProvider>
+      <Stack>
+        <Stack.Screen
+          name="index"
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="(auth)"
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="(tabs)"
+          options={{ headerShown: false }}
+        />
+        {/* <Stack.Screen
         name="/search/[query]"
         options={{ headerShown: false }}
       /> */}
-    </Stack>
+      </Stack>
+    </GlobalProvider>
   );
 };
 
